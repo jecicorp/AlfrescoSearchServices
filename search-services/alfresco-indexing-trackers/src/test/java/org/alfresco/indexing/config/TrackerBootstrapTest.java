ff43e3e4cc74b8ab159ef216e40778e5accb9588
Jérémie Lesage <jeremie.lesage@jeci.fr>
2026-06-13 13:23:38 +0200

    docs: use Jeci LGPL header in files created since the fork
    Replace the Alfresco license header (copied during scaffolding) with the
    standard Jeci SARL header on all source files authored since the JECI
    fork, keeping the project's GNU LGPL v3 license.

--- search-services/alfresco-indexing-trackers/src/test/java/org/alfresco/indexing/config/TrackerBootstrapTest.java
+++ search-services/alfresco-indexing-trackers/src/test/java/org/alfresco/indexing/config/TrackerBootstrapTest.java
@@ -1,27 +1,16 @@
 /*
- * #%L
- * Alfresco Indexing Trackers
- * %%
- * Copyright (C) 2005 - 2024 Alfresco Software Limited
- * %%
- * This file is part of the Alfresco software.
- * If the software was purchased under a paid Alfresco license, the terms of
- * the paid license agreement will prevail.  Otherwise, the software is
- * provided under the following open source license terms:
+ * Copyright 2026 - Jeci SARL - https://jeci.fr
  *
- * Alfresco is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
  *
- * Alfresco is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
- * #L%
+ * You should have received a copy of the GNU Lesser General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses/.
  */
 package org.alfresco.indexing.config;
 
